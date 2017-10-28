d4f6b990cf6a6f5b2e7780e813082759519656af
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-10-28 11:28:07 +0100

    connect to docker mongo

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://user:password@ds113925.mlab.com:13925/vms-spike';
+//const uri = 'mongodb://user:password@ds113925.mlab.com:13925/vms-spike';
+const uri = 'mongodb://store:27017';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
